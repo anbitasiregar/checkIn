eb71aa9d4a54090a8de3a9cb532b1a0510c319b8
anbitasiregar <anbitasiregar@gmail.com>
2015-06-17 14:54:08 -0400

    Initial Commit

---
+++ checkIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  checkIn
+//
+//  Created by Anbita Siregar on 6/17/15.
+//  Copyright (c) 2015 Anbita Siregar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
